3363caf18f4ec8b1c4ed00da45052f88bd33ac91
Jonas Pfenniger <jonas@pfenniger.name>
2012-09-11 19:07:52 +0100

    Removing unused AppCache for now

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,12 +4,9 @@
   // Tell the CSS that JS is available
   $("html").removeClass("no-js");
 
-
   // Prevent iOS scrolling
   document.ontouchmove = function(e) {e.preventDefault()};
 
-  // Application cache setup
-  var appCache = window.applicationCache;
   var spinner = new Spinner();
 
   function handleCacheEvent(e) {
@@ -19,25 +16,6 @@
   function handleCacheError(e) {
     console.log('error', arguments);
   };
-
-  // Fired after the first cache of the manifest.
-  appCache.addEventListener('cached', handleCacheEvent, false);
-  // Checking for an update. Always the first event fired in the sequence.
-  appCache.addEventListener('checking', handleCacheEvent, false);
-  // An update was found. The browser is fetching resources.
-  appCache.addEventListener('downloading', handleCacheEvent, false);
-  // The manifest returns 404 or 410, the download failed,
-  // or the manifest changed while the download was in progress.
-  appCache.addEventListener('error', handleCacheError, false);
-  // Fired after the first download of the manifest.
-  appCache.addEventListener('noupdate', handleCacheEvent, false);
-  // Fired if the manifest file returns a 404 or 410.
-  // This results in the application cache being deleted.
-  appCache.addEventListener('obsolete', handleCacheEvent, false);
-  // Fired for each resource listed in the manifest as it is being fetched.
-  appCache.addEventListener('progress', handleCacheEvent, false);
-  // Fired when the manifest resources have been newly redownloaded.
-  appCache.addEventListener('updateready', handleCacheEvent, false);
 
   // Sound setup
   $(function() {
