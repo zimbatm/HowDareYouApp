438b2d4d6243dd4e86b0472ec3122335c1c99b27
Jonas Pfenniger <zimbatm@zimbatm.com>
2013-10-12 15:44:56 +0100

    HowDareYou app is static. Use S3 to distribute it instead of Heroku.

--- Gemfile
+++ Gemfile
@@ -1,16 +1,4 @@
-source :rubygems
+source "https://rubygems.org"
 
-gem "middleman", "~>3.0.0"
-
-# For the heroku serving
-gem "thin"
-gem "rack-contrib"
-gem "rake"
-
-# optionally
-gem "rack-canonical-host"
-gem "rack-cache"
-
-group :development do
-  gem "rawler"
-end
+gem "middleman"
+gem "middleman-s3_sync"
--- config.rb
+++ config.rb
@@ -1,38 +1,3 @@
-### 
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-# 
-# With no layout
-# page "/path/to/file.html", :layout => false
-# 
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-# 
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
 ###
 # Helpers
 ###
@@ -40,12 +5,18 @@
 # Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'howdareyouapp'
+  s3_sync.region                     = 'us-east-1'
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET_ACCESS_KEY']
+  # We delete stray files by default.
+  s3_sync.delete                     = false
+  # We chain after the build step by default. This may not be your desired behavior...
+  s3_sync.after_build                = false
+  s3_sync.prefer_gzip                = true
+  s3_sync.reduced_redundancy_storage = false
+end
 
 # Version of HowDareYouApp
 set :my_version, '1.2'
@@ -60,26 +31,26 @@ set :images_dir, 'images'
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Cache invalidation
   #activate :cache_buster
   #
   activate :asset_hash
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
 
   # Compress files
-  #activate :gzip
-  
+  activate :gzip
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
